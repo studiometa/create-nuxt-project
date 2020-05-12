e509c860d3c49e2a7a512d7061c55d1589ad7c67
Titouan Mathis <titouan@studiometa.fr>
2020-05-12 15:28:44 +0200

    Update packages to their latest versions

--- template/pages/index.vue
+++ template/pages/index.vue
@@ -1,6 +1,11 @@
 <template>
-  <div class="flex items-center justify-center w-full h-screen">
-    <logo class="m-auto" />
+  <div class="flex items-center justify-center w-full h-screen text-center">
+    <div>
+      <logo class="m-auto" />
+      <h1 class="mt-4">
+        Hello world!
+      </h1>
+    </div>
   </div>
 </template>
 
--- template/tailwind.config.js
+++ template/tailwind.config.js
@@ -9,4 +9,16 @@ const config = require('@studiometa/tailwind-config');
  */
 module.exports = merge(config, {
   // Extends the default Studio Meta Tailwind configuration here...
+  purge: {
+    // Learn more on https://tailwindcss.com/docs/controlling-file-size/#removing-unused-css
+    enabled: process.env.NODE_ENV === 'production',
+    content: [
+      'components/**/*.vue',
+      'layouts/**/*.vue',
+      'pages/**/*.vue',
+      'plugins/**/*.js',
+      'utils/**/*.js',
+      'nuxt.config.js'
+    ]
+  }
 });
